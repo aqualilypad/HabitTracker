9622ff3cee642d59ec3eb49cceecb456eca0977c
WenjiaSong <63444209+WenjiaSong@users.noreply.github.com>
2021-07-22 19:22:04 -0700

    testing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,8 +14,10 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-    }
 
+
+    }
+//hello this is testing bla bla
 
     public static void main(String[] args) {
         launch(args);
